44c881f5fa39d6892a05dd34f4d863ca3e53e9a1
snapCheol <chief1689@naver.com>
2020-09-25 11:50:29 +0900

    초기 파일 정리  및 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import DetailPage from './pages/DetailPage';
+import Home from './pages/Home';
+import PostPage from './pages/PostPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route component={Home} path="/" exact />
+      <Route component={PostPage} path="/post" />
+      <Route component={DetailPage} path="/detail/:id" />
+    </>
   );
 }
 
